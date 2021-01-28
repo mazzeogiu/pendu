85a7b470969a117f0ce2789c450e6ef030e18829
Giuseppe <mazzeogiu@gmail.com>
2021-01-28 14:16:59 +0100

    check letter, check word, figure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,88 @@
+function design(nbrerror) {
+    let svg = document.getElementById('svg');
+    let output = '';
+    let figure =
+    [
+        '<line x1="50" y1="300" x2="10" y2="330" style="stroke:rgb(46, 35, 5);stroke-width:6" />',
+        '<line x1="55" y1="300" x2="55" y2="330" style="stroke:rgb(46, 35, 5);stroke-width:6" />',
+        '<line x1="60" y1="300" x2="100" y2="330" style="stroke:rgb(46, 35, 5);stroke-width:6" />',
+        '<rect x="50" y="0" width="10" height="300" style="fill:rgb(46, 35, 5);stroke-width:3;stroke:rgb(46, 35, 5)" />',
+        '<rect x="60" y="0" width="120" height="10" style="fill:rgb(46, 35, 5);stroke-width:3;stroke:rgb(46, 35, 5)" />',
+        '<line x1="60" y1="60" x2="110" y2="10" style="stroke:rgb(46,35, 5);stroke-width:6" />',
+        '<line x1="150" y1="10" x2="150" y2="80" style="stroke:rgb(46, 35, 5);stroke-width:3" />',
+        '<circle cx="150" cy="100" r="20" stroke="black" stroke-width="4" fill="grey" />',
+        '<line x1="150" y1="120" x2="150" y2="200" style="stroke:rgb(0, 0, 0);stroke-width:5" />',
+        '<line x1="150" y1="130" x2="125" y2="180" style="stroke:rgb(0, 0, 0);stroke-width:5" />',
+        '<line x1="150" y1="130" x2="175" y2="180" style="stroke:rgb(0, 0, 0);stroke-width:5" />',
+        '<line x1="150" y1="200" x2="130" y2="270" style="stroke:rgb(0, 0, 0);stroke-width:5" />',
+        '<line x1="150" y1="200" x2="170" y2="270" style="stroke:rgb(0, 0, 0);stroke-width:5" />',
+    ];
+
+    for (let i = 0; i < nbrerror; i++) {
+      output += figure[i];  
+    }
+    svg.innerHTML = output;        
+}
+
+function checkLetter(array, word) {
+    let output = ''
+    for (let c = 0; c < word.length; c++) {
+        if (array.includes(word[c])) {
+            output = output + word[c];
+        }else {
+            output = output + '_';
+        }
+    }
+    document.getElementById("word").innerHTML = output;
+}
+
 function loadDoc() {
     let xhttp = new XMLHttpRequest();
     
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let word = this.responseText.trim();
-            let screenWord = '';
+            let hiddenWord = '';
+            let nbrError = 0;
             let letters = [];
-
-            for (let i = 0; i < word.length; i++) {
-                screenWord += '_';
-                
-            }
-            console.log(word.length);
-            console.log(word);
-
-            document.getElementById("word").innerHTML = screenWord;
             let letter = document.getElementById("letter");
             let btn = document.getElementById("btn");
 
-            btn.addEventListener('click', function(){
-                console.log('btn');
+            //Inizialise word on screen
+            for (let i = 0; i < word.length; i++) {
+                hiddenWord += word[i] =='-' ? '-' : '_'; 
+            }
+            document.getElementById("word").innerHTML = hiddenWord;
+           
+            console.log(word);
+
+            //Add new choose letter
+            letter.addEventListener('click', function(){
+                hiddenWord = '';
+                letters.push(letter.value);
+
+                checkLetter(letters, word);
+                
+                if (!word.includes(letter.value)) {
+                    nbrError += 1;
+                    design(nbrError);
+                }
+                
             });
 
-            letter.addEventListener('click', function(){
-                screenWord = word;
-                document.getElementById("word").innerHTML = screenWord;
-                letters.push(letter.value);
-                //non funziona ancora
-                
-                    for (let c = 0; c < word.length; c++) {
-                        if (!letters.includes(word[c])) {
-                            screenWord = word.replace(word[c], '_');
-                        }
-                        //screenWord = letters.includes(word[c]) ?  word.replace(word[c], word[c]): word.replace(word[c], '_');
-                        console.log(letters);
-                    }
-                document.getElementById("word").innerHTML = screenWord;
+            //check solution
+            btn.addEventListener('click', function(){
+                let solution = document.getElementById("solution").value;
+
+
+                if(solution === word) {
+                    document.getElementById("word").innerHTML = word;
+                    console.log("win");
+                }else {
+                    nbrError += 1;
+                    design(nbrError);
+                    console.log("Lose + dessin")
+                }
             });
         }
     };
@@ -43,6 +90,8 @@ function loadDoc() {
     xhttp.open("POST", "http://localhost/Pendu/api/getData.php?tr=12345", true);
     xhttp.send();
 }
+
+
 
 //code
 loadDoc();
